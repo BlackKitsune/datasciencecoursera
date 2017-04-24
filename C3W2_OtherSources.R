a375e18d6ec7c3f330ff3ab50033bf4189964d6e
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-04-24 12:12:22 +0200

    Create C3W2_OtherSources.R

---
+++ C3W2_OtherSources.R
@@ -0,0 +1,28 @@
+#### C3W2 - Other sources
+
+## Packages in R: google "storage_mode R package"
+## Interact directly with files (?connections)
+#       file: open connection to a text file
+#       url: open a connection to a url
+#       gzfile: open connection to .gz file
+#       bzfile: open a connection to a .bz2 file
+# REMEMBER TO CLOSE CONNECTIONS
+
+## foreign package loads data from Minitab, S, SAS, SPSS, Stata, Systal
+#       read.arff(Weka)
+#       read.dta(Stata)
+#       read.mtp(Minitab)
+#       read.octave(Octave)
+#       read.spss(SPSS)
+#       read.xport(SAS)
+
+## Examples of other database packages:
+# RPostresSQL --> DBI compliant database connection from R
+# RODBC --> PostgreQL, MySQL, Microsoft Access and SQLite
+# RMongo, rmongodb --> MongoDb
+
+## Reading images: jpg, readbitmap, png, EBIimage
+
+## Reading GIS (Geographic Information System) data: rdgal, rgeos, raster
+
+## Reading music data: tuneR, seewave
