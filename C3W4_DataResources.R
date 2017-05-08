d07327897569984b5a10af2d2b1bed924ddb5cc7
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-05-07 18:10:21 -0600

    Create C3W4_DataResources.R

---
+++ C3W4_DataResources.R
@@ -0,0 +1,10 @@
+### C3W4 - Data resources
+
+# Open Government Sites
+# Gapminder has open datasets
+# Survey data from the US asdfree
+# Infochimps Marketplace
+# Kaggle
+# Collections of data scientists
+# Specialized collections
+# APIs with R interfaces (twitter, figshare, PLoS, rplos, facebook)
