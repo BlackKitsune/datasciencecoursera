62a18e0ce28d5434901e182d541dbf5724825d64
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-08-01 09:28:34 +0200

    Update C4W4-Assignment.R

--- C4W4-Assignment.R
+++ C4W4-Assignment.R
@@ -93,9 +93,130 @@ head(SCC)
 sub_balt <- subset(NEI, fips =="24510")
 head(sub_balt)
 
+# Emission per year per type
+type_balt <- aggregate(Emissions ~ year + type, 
+                       sub_balt, 
+                       sum)
+
 # Plot the data
-ggp <- ggplot(sub_balt,aes(factor(year),Emissions,fill=type)) 
-+      geom_bar(stat="identity")
-+      
+g <- ggplot(type_balt, aes(year, Emissions, color = type))
+g + geom_line() +
+    ggtitle("Total PM2.5 Baltimore (MD) Vs Year & Type") +
+    xlab("Year") + 
+    ylab("PM2.5 Emissions")
+
+# Save to png file
+dev.copy(png, "plot3.png", width = 480, height = 480)
+dev.off()
+
+### C4W4 - Exploratory data analysis IFG
+
+# Plot4.R: Emissions from coal combustion change
+# between 1999 and 2012
+
+# Read the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Take a look at the data
+View(NEI)
+View(SCC)
+
+# Take the coal combustion sources from SCC
+# ignore.case = TRUE case is ignored during matching
+coal  <- grepl("coal", SCC$Short.Name, ignore.case=TRUE)
+sub_SCC <- SCC[coal, ]
+
+# Merge the data from SCC and NEI
+mrg <- merge(NEI, sub_SCC, by = "SCC")
+
+# Total emissions per year
+total_pm25 <- tapply(mrg$Emissions, mrg$year, sum)
+
+# Plot with barplot
+barplot(total_pm25, mrg$year,
+        main = "Total PM2.5 Emissions from Coal",
+        xlab = "Year",
+        ylab = "PM2.5 Emissions",
+        col = "darkblue")
+
+# Save the plot
+dev.copy(png, "plot4.png", width = 480, height = 480)
+dev.off()
+
+### C4W4 - Exploratory data analysis IFG
+
+# Plot5.R: How have emissions from motor vehicle sources 
+# changed from 1999–2008 in Baltimore City?
+
+# Read the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Take a look at the data
+View(NEI)
+View(SCC)
+
+# Take the vehicle sources from SCC
+# ignore.case = TRUE case is ignored during matching
+vehicle  <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+sub_SCC <- SCC[vehicle, ]
+
+# Make subset of Baltimore city data
+sub_balt <- subset(NEI, fips =="24510" & type == "ON-ROAD")
+
+# Total emissions per year
+total_pm25 <- tapply(sub_balt$Emissions, sub_balt$year, sum)
+
+# Plot with barplot
+barplot(total_pm25, mrg$year,
+        main = "Total PM2.5 vehicle emissions in Baltimore",
+        xlab = "Year",
+        ylab = "PM2.5 Emissions",
+        col = "darkblue")
+
+# Save the plot
+dev.copy(png, "plot5.png", width = 480, height = 480)
+dev.off()
+
+### C4W4 - Exploratory data analysis IFG
+
+# Plot6.R: Compare emissions from motor vehicle sources 
+# in Baltimore City with emissions from motor vehicle sources 
+# in Los Angeles County, California (fips == "06037"). 
+# Which city has seen greater changes over time in motor vehicle emissions?
+
+library(ggplot2)
+
+# Read the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Take a look at the data
+View(NEI)
+View(SCC)
+
+# Make subset of Baltimore and LA data
+sub_balt <- subset(NEI, fips =="24510" & type == "ON-ROAD")
+sub_la <- subset(NEI, fips == "06037" & type == "ON-ROAD")
+
+# Add city column
+sub_balt$city <- "Baltimore"
+sub_la$city <- "Los Angeles"
+
+balt <- aggregate(Emissions ~ year + type + city, sub_balt, sum)
+la <- aggregate(Emissions ~ year + type + city, sub_la, sum)
+
+# Merge the data
+total_pm25 <- rbind(balt, la)
+
+# Plot 
+qplot(year, Emissions, data = total_pm25, color = city, geom = "line") +
+ggtitle("Total PM2.5 vehicle emissions in Baltimore and LA") +
+xlab = "Year" +
+ylab = "PM2.5 Emissions"
+
+dev.copy(png, "plot6.png", width = 480, height = 480)
+dev.off()
 
 
